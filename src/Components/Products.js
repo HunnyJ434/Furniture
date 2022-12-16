1905b5afd9a8c266a9ac985af8cf29d07f83ed2a
HunnyJ434 <fhunny@lakeheadu.ca>
2022-12-16 10:20:31 -0500

    first commit

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,32 +1,35 @@
 import '../App.css';
 
-export default function Services(){
-  async function getData() {
-    let url = 'https://fh-api-dev.herokuapp.com/api/products-service/products/website/CAD?page=0&limit=8';
-    try {
-        let res = await fetch(url);
-        let data = await res.json();
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
-}
+export default function Products(){
+  setTimeout(() => {
+    async function getData() {
+      let url = 'https://fh-api-dev.herokuapp.com/api/products-service/products/website/CAD?page=0&limit=8';
+      try {
+          let res = await fetch(url);
+          let data = await res.json();
+          return data;
+      } catch (error) {
+          console.log(error);
+      }
+  }
+  
+  async function renderData() {
+    let res = await getData();
+    let data = res.data.products;
+    let html = '';
+    data.forEach(item => {
+      html += `<div class="Items">
+      <img class ="item--img" src="${item.imageURLs[0]}" >
+      <p class="item--description">${item.fulhausProductName}</p>
+      <p class="item--price">$${item.retailPrice} ${item.orderCurrency}</p>
+      </div>`;
+      let container = document.querySelector('.products');
+      container.innerHTML = html;
+    })
+  }
+  renderData();
+  },500)
 
-async function renderData() {
-  let res = await getData();
-  let data = res.data.products;
-  let html = '';
-  data.forEach(item => {
-    html += `<div class="Items">
-    <img class ="item--img" src="${item.imageURLs[0]}" >
-    <p class="item--description">${item.fulhausProductName}</p>
-    <p class="item--price">$${item.retailPrice} ${item.orderCurrency}</p>
-    </div>`;
-    let container = document.querySelector('.products');
-    container.innerHTML = html;
-  })
-}
-renderData();
     return (
     <div className="products">
 
